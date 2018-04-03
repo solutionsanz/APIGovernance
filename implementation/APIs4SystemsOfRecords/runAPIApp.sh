3843e2658d615ec32c68ea70124173de0c8cf44a
Serene Tan <serene.tan@oracle.com>
2018-04-03 15:55:58 +1000

    Updated by Serene

--- implementation/APIs4SystemsOfRecords/runAPIApp.sh
+++ implementation/APIs4SystemsOfRecords/runAPIApp.sh
@@ -1,5 +1,6 @@
-#!/bin/ash
-export MONGODB_SERVER=mongo
-env | grep MONGO
+#export MONGODB_SERVER=mongo
+#env | grep MONGO
+cp swagger.yaml ../implementation/APIs4SystemsOfRecords/anki-sysrecords.yml
+cd ../implementation/APIs4SystemsOfRecords
+. setEnv.sh
 node app.js
-
